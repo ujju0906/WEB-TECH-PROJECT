aca8092a7ef0d5b7966662a1e9112d3596282aa9
ujju0906 <75610324+ujju0906@users.noreply.github.com>
2021-12-04 10:18:10 +0530

    Add files via upload
    while working put js files in src and html,pic files in public

---
+++ x.js
@@ -0,0 +1,9 @@
+var company = {
+    Name: "PES University", Address: "India",
+    Contact: "+919876543210", Email: "www.pes.edu"
+    };
+    // Display the object information
+    console.log("Information of variable company:", company);
+    // Display the type of variable
+    console.log("Type of variable company:", typeof company);
+    
